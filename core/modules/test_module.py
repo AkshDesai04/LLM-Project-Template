adeb6d5bbf6203f079ac41aa80458eff2e0ff966
Aksh Desai <aksh.cilans@gmail.com>
2026-08-03 12:22:35 +0530

    Cleaned up test_module.py

--- core/modules/test_module.py
+++ core/modules/test_module.py
@@ -3,11 +3,6 @@ from utils.io import read_prompt
 
 
 class FileSummaryPrompt(Base):
-    prompt: str = ""
+    prompt = read_prompt("test_prompt")
     model: str = "openai/o3-mini-2025-01-31"
-    stream: bool = False
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        if not self.prompt:
-            self.prompt = read_prompt("test_prompt")
+    stream: bool = False
